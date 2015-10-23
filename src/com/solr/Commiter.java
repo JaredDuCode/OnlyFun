7a286617a09589529796a04dbe9f5207fa7a75e2
JaredDu <JaredDu@163.com>
2015-10-23 11:40:47 +0800

    Revert "commit2"
    This reverts commit ba2fd6c197043f17abf5ee4e86208052217ada1c.

--- src/com/solr/Commiter.java
+++ src/com/solr/Commiter.java
@@ -33,8 +33,7 @@ public class Commiter {
 		
 	}
 	
-	public  void query(String queryStr) throws SolrServerException
-	{
+	public  void query(String queryStr) throws SolrServerException{
 		SolrQuery solrQuery = new SolrQuery(queryStr);  
 		solrQuery.setRows(5);  
         QueryResponse resp = solrServer.query(solrQuery);  
